1972cc967116800bb69e790fa826f46e1c627769
Dmitry Rodnikov <dim.reg@mail.ru>
2020-04-26 09:40:46 +0300

    create shadow grid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Cell } from './cell';
+import './app.css';
+
+type Point = [number, number];
+
+const initialGrid = [
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+  [0,0,0,0,0,0],
+];
+const initialPoint: Point = [0,0];
+
+function getShadowAngle(rowDelta: number, columnDelta: number): number {
+  let absAngle = Math.atan(columnDelta/rowDelta) * 180 / Math.PI;
+  return rowDelta < 0 ? -absAngle : 180 - absAngle;
+}
 
 function App() {
+  const [selected, setSelected] = useState<Point>(initialPoint);
+  const [grid, setGrid] = useState(initialGrid);
+
+  const updateGrid = useCallback((target: Point) => {
+    setGrid(grid => grid.map(
+      (row, rowIndex) => row.map(
+        (_, columnIndex) => {
+          const isSelected = target[0] === rowIndex && target[1] === columnIndex;
+          const rowDistance = rowIndex - target[0];
+          const colDistance = columnIndex - target[1];
+          const angle = isSelected ? 0 : getShadowAngle(rowDistance, colDistance);
+          return angle < 0 ? 360 + angle : angle;
+        }
+      )
+    ));
+    setSelected(target);
+  }, []);
+
+  useEffect(() => {
+    updateGrid(initialPoint);
+  }, [updateGrid]);
+
+  const handleClick = (target: Point) => {
+    updateGrid(target);
+  }
+
+  const gridComponent = grid.map((_, rowIndex) => {
+    return (
+      <div key={rowIndex} className="grid-row">
+        {grid[rowIndex].map((value, columnIndex) => {
+          return (
+            <Cell
+              key={columnIndex} 
+              value={value}
+              rowIndex={rowIndex} 
+              columnIndex={columnIndex} 
+              selected={selected}
+              onClick={handleClick}
+            />
+          );
+        })}
+      </div>
+    );
+  });
+
+  const handleKeyDown = useCallback((e: React.KeyboardEvent) => {
+    switch (e.key) {
+      case 'ArrowUp': {
+        if (selected[0] === 0) return;
+        updateGrid([selected[0] - 1, selected[1]]);
+        break;
+      }
+      case 'ArrowDown': {
+        if (selected[0] === grid.length - 1) return;
+        updateGrid([selected[0] + 1, selected[1]])
+        break;
+      }
+      case 'ArrowLeft': {
+        if (selected[1] === 0) return;
+        updateGrid([selected[0], selected[1] - 1])
+        break;
+      }
+      case 'ArrowRight': {
+        if (selected[1] === grid[0].length - 1) return;
+        updateGrid([selected[0], selected[1] + 1])
+        break;
+      } 
+    }
+  }, [updateGrid, selected, grid]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+      className="grid-wrapper" 
+      tabIndex={0} 
+      onKeyDown={handleKeyDown}
+    >
+      <div>
+        {gridComponent}
+      </div>
     </div>
   );
 }
---
+++ src/cell.tsx
@@ -0,0 +1,83 @@
+import React, { useCallback } from 'react';
+import './cell.css';
+
+const SELECTED_COLOR = '#fff';
+const BASE_COLOR = '#ffffff20';
+const CELL_SIZE = '50px';
+const CIRCLE_SIZE = '12px';
+
+interface CellShadowProps {
+  angle: number;
+  isSelected: boolean;
+  shadowLength: number;
+  shadowBlur: number;
+  shadowSize: number;
+  opacity: number;
+}
+
+const CellShadow = ({angle, isSelected, shadowLength, shadowBlur, shadowSize, opacity}: CellShadowProps) => {    
+  return (
+    <div
+      className="cell-dot"
+      style={{
+        width: CIRCLE_SIZE,
+        height: CIRCLE_SIZE,
+        boxShadow: isSelected 
+          ? '0 0 100px 40px rgba(255,255,255,0.3)' 
+          : `0 -${shadowLength}px ${shadowBlur}px ${shadowSize}px rgba(0,0,0,${opacity})`,
+        transform: `rotate(${angle}deg)`,
+      }}
+    />
+  );
+}
+
+interface CellProps {
+  rowIndex: number;
+  value: number;
+  columnIndex: number; 
+  selected: [number, number]; 
+  onClick: (target: [number, number]) => void;
+}
+
+export const Cell = ({rowIndex, value, columnIndex, selected, onClick}: CellProps) => {  
+  const rowDistance = rowIndex - selected[0];
+  const colDistance = columnIndex - selected[1];
+  const absDistance = Math.max(Math.abs(rowDistance), Math.abs(colDistance));
+  const shadowLength = 5 + 5 * absDistance;
+  const shadowBlur = 5 + Math.pow(absDistance, 2);
+  const shadowSize = 1 + absDistance;
+  const opacity = 1 / (1 + absDistance * 2);
+  const isSelected = selected[0] === rowIndex && selected[1] === columnIndex;
+
+  const handleClick = useCallback(() => {
+    onClick([rowIndex, columnIndex]);
+  }, [onClick, rowIndex, columnIndex]);
+
+  return (
+    <div 
+      className="cell-container"
+      style={{
+        width: CELL_SIZE,
+        height: CELL_SIZE,
+      }}
+    >
+      <div
+        onClick={handleClick}
+        className="cell"
+        style={{
+          width: CIRCLE_SIZE,
+          height: CIRCLE_SIZE,
+          background: isSelected ? SELECTED_COLOR : BASE_COLOR,
+        }}
+      />
+      <CellShadow 
+        isSelected={isSelected} 
+        angle={value} 
+        shadowBlur={shadowBlur}
+        shadowSize={shadowSize}
+        shadowLength={shadowLength} 
+        opacity={opacity} 
+      />
+    </div>
+  );
+}
