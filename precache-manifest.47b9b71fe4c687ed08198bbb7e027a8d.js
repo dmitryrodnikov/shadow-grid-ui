e348216ae04f9554323a6d2f53d6c915adab5888
Dmitry Rodnikov <dim.reg@mail.ru>
2020-04-26 11:05:01 +0300

    Updates

---
+++ precache-manifest.47b9b71fe4c687ed08198bbb7e027a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5daaf586a7f2a49978e4114473048d97",
+    "url": "/shadow-grid-ui/index.html"
+  },
+  {
+    "revision": "b37889d194d8f9625d3d",
+    "url": "/shadow-grid-ui/static/css/main.676c6848.chunk.css"
+  },
+  {
+    "revision": "e5841a235d79d4ce7ca7",
+    "url": "/shadow-grid-ui/static/js/2.20f41b78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shadow-grid-ui/static/js/2.20f41b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37889d194d8f9625d3d",
+    "url": "/shadow-grid-ui/static/js/main.b807e0ca.chunk.js"
+  },
+  {
+    "revision": "802cd8534fde36246e28",
+    "url": "/shadow-grid-ui/static/js/runtime-main.db5a8079.js"
+  }
+]);
